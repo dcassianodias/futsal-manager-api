31645bbfff42301815758d324ecfc662bc4642f0
Danilo Dias <dcassianodias@gmail.com>
2026-03-08 18:19:03 -0300

    feat: adição de busca no repository de usuario

--- src/main/java/com/futsalmanager/infrastructure/repositories/UsuarioRepository.java
+++ src/main/java/com/futsalmanager/infrastructure/repositories/UsuarioRepository.java
@@ -4,8 +4,11 @@ import com.futsalmanager.domain.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    List<Usuario> findByTimeId(UUID timeId);
 }
