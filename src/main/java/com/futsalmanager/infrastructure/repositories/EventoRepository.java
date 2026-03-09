8067d158f1087cd1d8f0bda6449998f40df7cb5d
Danilo Dias <dcassianodias@gmail.com>
2026-03-08 23:01:12 -0300

    feat: criacao de repository de Evento

--- src/main/java/com/futsalmanager/infrastructure/repositories/EventoRepository.java
+++ src/main/java/com/futsalmanager/infrastructure/repositories/EventoRepository.java
@@ -3,7 +3,11 @@ package com.futsalmanager.infrastructure.repositories;
 import com.futsalmanager.domain.entities.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EventoRepository extends JpaRepository<Evento, UUID> {
+
+    List<Evento> findByTimeIdOrderByDataInicioDesc(UUID timeId);
+
 }
