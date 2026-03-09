0679952dd7657d9a9062bdfa36869da3ea8c1c76
Danilo Dias <dcassianodias@gmail.com>
2026-03-08 23:00:24 -0300

    feat: criacao de controller de Evento

---
+++ src/main/java/com/futsalmanager/api/controller/EventoController.java
@@ -0,0 +1,69 @@
+package com.futsalmanager.api.controller;
+
+import com.futsalmanager.api.dto.request.EventoCreateRequest;
+import com.futsalmanager.api.dto.request.EventoUpdateRequest;
+import com.futsalmanager.api.dto.response.EventoResponse;
+import com.futsalmanager.application.services.EventoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/evento/v1")
+public class EventoController {
+
+    private final EventoService service;
+
+    public EventoController(EventoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public EventoResponse findById(@PathVariable UUID id){
+        return service.findById(id);
+    }
+
+    @GetMapping
+    public List<EventoResponse> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/time/{timeId}")
+    public ResponseEntity<List<EventoResponse>> findByTime(@PathVariable UUID timeId){
+        List<EventoResponse> list = service.findByTime(timeId);
+        if (list.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventoResponse> create(@RequestBody EventoCreateRequest request){
+        EventoResponse created = service.create(request);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventoResponse> update(@PathVariable UUID id, @RequestBody EventoUpdateRequest request){
+        return ResponseEntity.ok(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
