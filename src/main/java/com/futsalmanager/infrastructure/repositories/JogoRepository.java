4ecff4c9465aa02f04521e99cb8cf4e97287675a
Danilo Dias <dcassianodias@gmail.com>
2026-03-18 17:04:28 -0300

    feat: criaçao de repository de jogo

--- src/main/java/com/futsalmanager/infrastructure/repositories/JogoRepository.java
+++ src/main/java/com/futsalmanager/infrastructure/repositories/JogoRepository.java
@@ -4,8 +4,23 @@ import com.futsalmanager.domain.entities.Jogo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface JogoRepository extends JpaRepository<Jogo, UUID> {
+
+    List<Jogo> findByTimeIdOrderByDataHoraDesc(UUID timeId);
+
+    boolean existsByTimeIdAndAdversarioAndLocalAndDataHora(UUID timeId, String adversario, String local,
+                                                           LocalDateTime dataHora);
+
+    boolean existsByTimeIdAndAdversarioAndLocalAndDataHoraAndIdNot(
+            UUID timeId,
+            String adversario,
+            String local,
+            LocalDateTime dataHora,
+            UUID id
+    );
 }
