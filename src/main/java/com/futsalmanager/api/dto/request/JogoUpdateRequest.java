e43ccd2ead298060977c33f5a8008cef13c991e1
Danilo Dias <dcassianodias@gmail.com>
2026-04-05 23:31:53 -0300

    feat: remoção de obrigatoriedade de campos de atualização

--- src/main/java/com/futsalmanager/api/dto/request/JogoUpdateRequest.java
+++ src/main/java/com/futsalmanager/api/dto/request/JogoUpdateRequest.java
@@ -11,17 +11,13 @@ import java.util.UUID;
 
 public record JogoUpdateRequest(
 
-        @NotNull(message = "Time mandante é obrigatório.")
         UUID timeId,
 
-        @NotBlank(message = "Nome do adversário é obrigatório.")
         String adversario,
 
-        @NotBlank(message = "Local do jogo é obrigatório.")
         String local,
 
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-        @NotNull(message = "Data e hora do jogo são obrigatórios.")
         LocalDateTime dataHora,
 
         StatusJogo status,
