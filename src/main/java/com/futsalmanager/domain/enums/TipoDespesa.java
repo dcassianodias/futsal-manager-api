2d8023828eb176c119a27fa21007b24c558d2108
Danilo Dias <dcassianodias@gmail.com>
2026-04-05 23:36:01 -0300

    feat: adição de campos de swagger

--- src/main/java/com/futsalmanager/domain/enums/TipoDespesa.java
+++ src/main/java/com/futsalmanager/domain/enums/TipoDespesa.java
@@ -1,10 +1,19 @@
 package com.futsalmanager.domain.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Tipos de despesas do time")
 public enum TipoDespesa {
 
+    @Schema(description = "Custo com aluguel da quadra")
     ALUGUEL_QUADRA,
-    UNIFORME,
-    EVENTO,
-    OUTROS
 
+    @Schema(description = "Compra de uniformes")
+    UNIFORME,
+
+    @Schema(description = "Custos relacionados a eventos")
+    EVENTO,
+
+    @Schema(description = "Outros tipos de despesas")
+    OUTROS
 }
