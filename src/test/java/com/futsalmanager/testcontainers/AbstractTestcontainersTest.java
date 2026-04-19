151e8f5663d566c8cb66cce89e3b931ac29c12aa
Danilo Dias <dcassianodias@gmail.com>
2026-04-19 16:47:07 -0300

    test: criação de teste containers

--- src/test/java/com/futsalmanager/testcontainers/AbstractTestcontainersTest.java
+++ src/test/java/com/futsalmanager/testcontainers/AbstractTestcontainersTest.java
@@ -4,6 +4,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * Classe base para testes que usam TestContainers com PostgreSQL.
@@ -13,17 +16,24 @@ import org.springframework.test.context.ContextConfiguration;
  * Se Docker não estiver disponível, use application-test.yml com H2.
  */
 @SpringBootTest
+@Testcontainers
 @ContextConfiguration(initializers = AbstractTestcontainersTest.PostgresInitializer.class)
 public abstract class AbstractTestcontainersTest {
+
+    @Container
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.12")
+            .withDatabaseName("futsaldb")
+            .withUsername("futsaluser")
+            .withPassword("futsalpass");
 
     public static class PostgresInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
-            // Usar PostgreSQL externo (assumindo que está rodando em localhost:5432)
-            applicationContext.getEnvironment().getSystemProperties().put("spring.datasource.url", "jdbc:postgresql://localhost:5432/futsaldb");
-            applicationContext.getEnvironment().getSystemProperties().put("spring.datasource.username", "futsaluser");
-            applicationContext.getEnvironment().getSystemProperties().put("spring.datasource.password", "futsalpass");
+            postgres.start();
+            applicationContext.getEnvironment().getSystemProperties().put("spring.datasource.url", postgres.getJdbcUrl());
+            applicationContext.getEnvironment().getSystemProperties().put("spring.datasource.username", postgres.getUsername());
+            applicationContext.getEnvironment().getSystemProperties().put("spring.datasource.password", postgres.getPassword());
             applicationContext.getEnvironment().getSystemProperties().put("spring.datasource.driver-class-name", "org.postgresql.Driver");
             applicationContext.getEnvironment().getSystemProperties().put("spring.jpa.hibernate.ddl-auto", "create-drop");
             applicationContext.getEnvironment().getSystemProperties().put("spring.flyway.enabled", "true");
